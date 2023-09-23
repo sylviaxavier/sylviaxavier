d9bb451028935e1fd2817cd456819c379af49f9d
Sylvia Xavier <xaviersylvia588@gmail.com>
2023-09-23 13:22:18 -0300

    add folders, HTML structure and carousel

---
+++ js/script.js
@@ -0,0 +1,30 @@
+let swiperCards = new Swiper('.card-content', {
+    // Optional parameters
+    //direction: 'vertical',
+    loop: true,
+    spaceBetween:32,
+    grabCursor: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable:true,
+      dynamicBullets: true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints:{
+      600:{
+        slidesPerView:2,
+      },
+      968:{
+        slidesPerView:3,
+      },
+    },
+
+});
