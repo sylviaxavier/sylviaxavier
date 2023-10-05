ec02502f818d2ae8f5071d98f1704641e945f68b
Sylvia Xavier <xaviersylvia588@gmail.com>
2023-10-05 08:26:21 -0300

    add cv, create home and formation pages

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
+window.addEventListener('scroll',reveal);
+function reveal(){
+  var reveals = document.querySelectorAll('.reveal')
+  
+  for(var i = 0; i < reveals.length; i++){
+    var windowheight = window.innerHeight;
+    var revealtop = reveals[i].getBoundingClientRect().top;
+    var revealpoint = 100;
+
+    if(revealtop < windowheight - revealpoint){
+      reveals[i].classList.add('active');
+    } else{
+      reveals[i].classList.remove('active');
+    }
+  }
+}
+
+window.addEventListener('scroll', navFluid)
+function navFluid(){
+  var header = document.querySelector("header")
+  header.classList.toggle('sticky', window.scrollY > 600)
+}
 //Swiper Script
 let swiperCards = new Swiper('.card-content', {
     // Optional parameters
